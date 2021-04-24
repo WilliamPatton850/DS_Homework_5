f3fbe5be9e967c8a58bc0900930dbe8538c4b51b
William Patton <wcp5@students.uwf.edu>
2021-04-24 18:41:00 -0500

    commit 2

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <cmath> 
 using namespace std;
 
+/*
 int gcdExtended(int a, int b, int &x, int &y){
     if(b == 0){
       x = 1;
@@ -14,40 +15,90 @@ int gcdExtended(int a, int b, int &x, int &y){
     y = x1 - (a / b) * y1;
     return gcd;
   }
+ */
+
+int gcdExtended(int a, int b){
+  if(b == 0){
+    return a;
+  }
+  return gcdExtended(b, a % b);
+}
+
+bool isPrime(int n){
+  for(int i = 2; i * i <= n; ++i){
+    if(n % i == 0){
+      return 0;
+    }
+  }
+  return 1;
+}
+
+int decrypt(int a, int b, int p){
+  if(b == 0){
+    return 1;
+  }
+  int r = decrypt(a, (b/2), p);
+  r = (r * r) % p;
+  if(b % 2){
+    r = (r * a) % p;
+  }
+  return r;
+}
 
 int main(){
 
-int n = 598115629;
-int e = 71;
+int n = 0; //598115629;
+int e = 0; //71;
+int cipherText = 0;
 
-int p = 17791;
-int q = n / p;
-int l;
-int totient = ((p-1)*(q-1));
+int p = 0; 
+int q = 0; 
+//int l;
+//int totient = ((p-1)*(q-1));
 //int n = (p*q);
 //int e = 3; 
-int x, y;
-int gcd;
-int a = totient;  
-int b = e;
+//int x, y;
+//int gcd;
+//int a = totient;  
+//int b = e;
 int d;
+
 
 cout << "Enter value for n: ";
 cin >> n;
-cout << endl;
 cout << "Enter value for e: ";
 cin >> e;
-cout << endl;
+
+for(int i = 2; i * i <= n; ++i){
+  if(n % i == 0){
+    if(isPrime(i) && isPrime(n / i)){
+      p = i;
+      q = n / i;
+      break;
+    }
+  }
+}
 
 
-
+/*
   if(p > q){
     l = p;
   }
   else{
     l = q;
   }
+*/
 
+int totient = ((p-1)*(q-1));
+
+  while(1){
+    if(gcdExtended(e, totient) == 1){
+      break;
+    }
+    ++e;
+  }
+
+/*
   while(1){
     if(l%(p-1) == 0 && l%(q-1) == 0){
       //cout << "The LCM of " << p << " and " << q << " is " << l << endl;
@@ -55,52 +106,47 @@ cout << endl;
     }
     l++;
   }
-
-/*
-cout << "p = " << p << endl;
-cout << "q = " << q << endl;
-cout << "l = " << l << endl;
-cout << "Totient = " << totient << endl;
-cout << "e = " << e << endl;
-cout << "n = " << n << endl;
 */
-
+/*
 a = l; 
 if(a < b) swap(a, b);
 gcd = gcdExtended(a, b, x, y); 
-//cout << "gcd = " << gcd << endl;
-//cout << "x = " << x << endl;
-//cout << "y = " << y << endl;
+
 if(y < 0){
   d = totient + y;
 }
 else{
   d = y; 
 }
-//cout << "d = " << d << endl;
+*/
+
+
+int z = 2;
+d = (1 + z * totient) / e;
 
 
 cout << "Public Key: (" << n << ", " << e << ")"  << endl;
 cout << "Private Key: (" << n << ", " << d << ")"  << endl;
 
-int cipherText = 576148263;
-long long z = pow(cipherText, d);
-long long birthYear = z % n;
+cout << "Enter cipher text: ";
+cin >> cipherText;
+//long long z2 = pow(cipherText, d);
+int birthYear = decrypt(cipherText, d, n);
+cout << "Decryption key: " << d << endl;
 cout << "Birth Year: " << birthYear << endl;
 
-gcd = gcdExtended(p, q, x, y);
-//cout << "gcd = " << gcd << endl;
-//cout << "x = " << x << endl;
-//cout << "y = " << y << endl;
+
+//gcd = gcdExtended(p, q, x, y);
+
+/*
 if(y < 0){
   d = totient + y;
 }
 else{
   d = y;
 }
-//cout << "d = " << d << endl;
-cout << "Public Key: (" << n << ", " << e << ")"  << endl;
-cout << "Private Key: (" << n << ", " << d << ")"  << endl;
+*/
+
 
   return 0;
 }
